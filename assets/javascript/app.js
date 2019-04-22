43893cc79ad8fe648e203b354ea12fd4d2337f31
Jo <jo.mtz133@gmail.com>
2019-04-22 17:48:38 -0500

    - Project updated from Google-places-in-cards branch - Firebase Authentication functions added - Data persistance with realtime Database

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,56 +1,383 @@
-var service = new google.maps.places.PlacesService($('#tag-id').get(0));
+// This example requires the Places library. Include the libraries=places
+// parameter when you first load the API. For example:
+// <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCuAnrF-SWPMrbAPVUjtNg9urdaaneF08c&libraries=places">
+var map;
+var infowindow;
+var service;
+var lattitude = 0;
+var longitude = 0;
+var autoComplete;
+var directionsService;
+var directionsDisplay;
+var inResultScreen = false;
+$(document).ready(function () {
+    //init firebase
+    var auth = firebase.auth();
 
-function placeQuery(){
-    var location = new google.maps.LatLng(22.154248,-100.979476)
-    var request = {
-        keyword: "pizza",
-        radius: 1500,
-        type: "restaurant",
-        location: location
+    $('#bt_login').on("click", function () {
+        const email = $('#email').val();
+        const password = $('#password').val();
+        auth.signInWithEmailAndPassword(email, password).then(function (result) {
+          //LOGIN OK
+        }).catch(function(error) {
+            console.log("errorCode", error.code);
+            console.log("errorMessage", error.message);
+        });
+       /* auth.createUserWithEmailAndPassword(email, password).then(function (result) {
+            //USER CREATED OK
+            console.log("USER CREATED IN AUTH");
+            //save user in database
+            const usuario = {
+                uid: result.user.uid,
+                email: result.user.email,
+            };
+            firebase.database().ref('users').child(result.user.uid).set(usuario).then(function () {
+                console.log("USER CREATED IN DB")
+            }).catch(function (error) {
+                console.log("ERROR", error);
+            });
+        }).catch(function (error) {
+            console.log("errorCode", error.code);
+            console.log("errorMessage", error.message);
+        });*/
+    });
+
+
+    firebase.auth().onAuthStateChanged(function(user) {
+        if (user) {
+            // User is signed in.
+            var displayName = user.displayName;
+            var email = user.email;
+            var emailVerified = user.emailVerified;
+            var photoURL = user.photoURL;
+            var isAnonymous = user.isAnonymous;
+            var uid = user.uid;
+            var providerData = user.providerData;
+            console.log(email)
+            // ...
+            //check if the user has last location saved
+            firebase.database().ref('users').child(uid).once('value').then(function (snapshot) {
+                const userData = snapshot.val();
+                if (userData.lat && userData.lng) {
+                    lattitude = userData.lat;
+                    longitude = userData.lng;
+                    var latLng = new google.maps.LatLng(userData.lat, userData.lng);
+                    var geocoder = new google.maps.Geocoder();
+                    var address;
+                    geocoder.geocode({ 'latLng': latLng }, function (results, status) {
+                        console.log(results[0].formatted_address);
+                        address = results[0].formatted_address;
+                        $("#location").val(address);
+                    });
+                } else {
+                    console.log("NO LAST LOCATION");
+                }
+            });
+        } else {
+            // User is signed out.
+            // ...
+        }
+    });
+
+    $("#location").val("");
+    $("#place").val("");
+    $("#email").val("correo@correo.com");
+    $("#password").val("correo123");
+
+
+    // Geolocate();
+    ActivateAutoComplete();
+
+    $(document.body).on("click", ".header-logo", function () {
+        $("#location").val("");
+        $("#place").val("");
+        location.reload();
+        inResultScreen = false;
+    });
+
+    $(document.body).on("click", "#search", function (event) {
+
+        // Prevent default action of button
+        event.preventDefault();
+
+        var search = "";
+
+        if (lattitude == 0 || longitude == 0) {
+            $("#no-location-modal").modal();
+            return;
+        }
+
+        // Clear card content for new refill
+        $("#cards").empty();
+
+
+        // Check if we are in the home screen or in the result screen
+        if (!inResultScreen) {
+            // Save search word in a variable
+            search = $("#place").val().trim();
+            $("#place-result").val(search);
+        } else {
+            search = $("#place-result").val().trim();
+        }
+
+        if (search == "") {
+            $("#no-search-modal").modal();
+            return;
+        }
+
+
+        $(".main-content").removeAttr("hidden");
+
+        $(".full-width").attr("hidden", "true");
+        $(".carousel").attr("hidden", "true");
+
+        inResultScreen = true;
+        // Service and render for route calculation
+        directionsService = new google.maps.DirectionsService();
+        directionsDisplay = new google.maps.DirectionsRenderer();
+
+
+        // Set location
+        console.log("coordinates: " + lattitude + " " + longitude);
+        var location = new google.maps.LatLng(lattitude, longitude);
+
+        infowindow = new google.maps.InfoWindow();
+
+        // Init google map && set the map to the display route service
+        map = new google.maps.Map(document.getElementById('map'), { center: location, zoom: 15 });
+        directionsDisplay.setMap(map);
+
+        // Create Location Marker
+        var myLatLng = { lat: lattitude, lng: longitude };
+        var homeIcon = {
+            url: "assets/images/location.png", // url
+            scaledSize: new google.maps.Size(40, 40), // scaled size
+        };
+        var markerHome = new google.maps.Marker({
+            position: myLatLng,
+            map: map,
+            animation: google.maps.Animation.DROP,
+            icon: homeIcon,
+            title: "Current Location"
+        });
+
+        // Add listener for clicking the marker
+        google.maps.event.addListener(markerHome, 'click', function () {
+            infowindow.setContent(markerHome.title);
+            infowindow.open(map, this);
+        });
+
+        // Set up of request for google places
+        var request = {
+            location: location,
+            radius: 8000,
+            keyword: search,
+            fields: ['name', 'formatted_address', 'url', 'rating']
+        };
+
+        service = new google.maps.places.PlacesService(document.createElement('div'));
+
+        service.nearbySearch(request, function (results, status) {
+
+            console.log("Number of results: " + results.length);
+            // console.log(results);
+
+            if (results.length == 0) {
+                var noResultsDiv = $("<div>");
+                var noResultsText = $("<h2>");
+                noResultsText.text("No Results Found, try again with a different parameter");
+                noResultsDiv.append(noResultsText);
+                $("#cards").append(noResultsDiv);
+            }
+            for (var i = 0; i < results.length; i++) {
+                // console.log(results[i]);
+                CreateMarker(results[i]);
+                CreateCard(results[i]);
+            }
+        });
+    });
+
+
+    $("#get-location").on("click", function (event) {
+        // Prevent default action of button
+        event.preventDefault();
+        Geolocate();
+
+    });
+
+    // Create each card with the information from the request
+
+
+});
+
+function CreateCard(data) {
+
+
+    // Creates the main div where we append everything
+    var mainDiv = $("<div>");
+    mainDiv.addClass("col-sm-6 col-md-4 col-lg-3 mb-3");
+
+    // Card div
+    var cardDiv = $("<div>");
+    cardDiv.addClass("card .z-depth-1-half");
+
+    // Image of the card
+    var img = $("<img>");
+    img.addClass("card-img-top");
+    img.attr("style", "height: 250px; object-fit: cover;");
+
+    // If the data doesn't have image, we use a placeholder of no image
+    if (data.photos != undefined) {
+        img.attr("src", data.photos[0].getUrl({ maxWidth: 250, maxHeight: 250 }));
+    } else {
+        img.attr("src", "assets/images/no-image.jpg");
     }
 
-    service.nearbySearch(request, function(results, status){
-        console.log(status)
-        if (status === google.maps.places.PlacesServiceStatus.OK){
-            for(var i = 0; i < results.length; i++){
-                var item = $('<h1 class="item">');
-                var p = $("<p>").text(results[i].name);
-                item.append(p);
+    // Card body div
+    var cardBodyDiv = $("<div>");
+    cardBodyDiv.addClass("card-body");
 
-                $("#tag-id").append(item)
+    // Card Name
+    var name = $("<h4>");
+    name.text(data.name);
 
-            }
-            console.log(results)
-            $("#header1").text(results[0].geometry.location.lat)
+    //Card address and icon
+    var address = $("<p>");
+    var addressIcon = $("<i class='fas fa-map-marker-alt'></i>");
+    address.text(data.vicinity);
+    address.prepend(addressIcon);
 
-            var placeID = results[0].place_id;
+    // Card rating and icon
+    var rate = $("<p>");
+    var rateIcon = $("<i class='fas fa-star'></i>");
+    rate.text(data.rating);
+    rate.prepend(rateIcon);
 
-            var request2 = {
-                placeId: placeID
-            }
+    //Card button with function of get route and icon
+    var button = $("<button>");
+    var buttonIcon = $("<i class='fas fa-car icon-white'></i>");
+    button.addClass("btn btn-lg btn-rounded btn-floating purple-gradient btn-block text-white");
+    button.attr("onclick", "CalcRoute(" +
+        lattitude + "," +
+        longitude + "," +
+        data.geometry.location.lat() + "," +
+        data.geometry.location.lng() + ");return false;");
+    button.text("Get route");
+    button.prepend(buttonIcon);
 
-            service.getDetails(request2, function(place, status){
-                if (status === google.maps.places.PlacesServiceStatus.OK){
-                    console.log(place)
-                }
-            })
+    // Appending of the card information
+    cardBodyDiv.append(name);
+    cardBodyDiv.append(address);
+    cardBodyDiv.append(rate);
+    cardBodyDiv.append(button);
+
+    // Appending of the image and the body
+    cardDiv.append(img);
+    cardDiv.append(cardBodyDiv);
+
+    // Appending of everything to the main card
+    mainDiv.append(cardDiv);
+
+    // Appending of each card to the main div
+    $("#cards").append(mainDiv);
+}
+
+// Geolocalization that asking user if want to share location and if geolocation is not enabled/available we create the location form
+function Geolocate() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(GetPosition);
+    } else {
+        CreateLocationForm();
+    }
+}
+
+// Save the lattitude and longitude from the geolocalization
+function GetPosition(position) {
+    lattitude = position.coords.latitude;
+    longitude = position.coords.longitude;
+    console.log("coordinates: " + lattitude + " " + longitude);
+    var latLng = new google.maps.LatLng(lattitude, longitude);
+    var geocoder = new google.maps.Geocoder();
+    var address;
+    geocoder.geocode({ 'latLng': latLng }, function (results, status) {
+        console.log(results[0].formatted_address);
+        address = results[0].formatted_address;
+        $("#location").val(address);
+    });
+
+    if(firebase.auth().currentUser){
+        //guardar última ubicación en database
+        const usuario = {
+            uid: firebase.auth().currentUser.uid,
+            email: firebase.auth().currentUser.email,
+            lat: position.coords.latitude,
+            lng: position.coords.longitude
+        };
+        firebase.database().ref('users').child(firebase.auth().currentUser.uid).update(usuario).then(function () {
+            console.log("REGISTRO USER LOCATION OK");
+        }).catch(function (error) {
+            console.log("ERROR SAVING USER LOCATION", error);
+        });
+    }else{
+        console.log("No hay sesion")
+    }
+
+}
+
+
+// Enables auto-complete
+function ActivateAutoComplete() {
+    var input = document.getElementById('location');
+    autoComplete = new google.maps.places.Autocomplete(input);
+    autoComplete.setFields(
+        ['address_components', 'geometry', 'icon', 'name']);
+
+    autoComplete.addListener('place_changed', function () {
+        var place = autoComplete.getPlace();
+        console.log(place);
+
+        var address = '';
+        if (place.address_components) {
+            address = [
+                (place.address_components[0] && place.address_components[0].short_name || ''),
+                (place.address_components[1] && place.address_components[1].short_name || ''),
+                (place.address_components[2] && place.address_components[2].short_name || '')
+            ].join(' ');
         }
-    })
+
+        lattitude = place.geometry.location.lat();
+        longitude = place.geometry.location.lng();
+
+    });
 }
 
-placeQuery()
-
-
-function testData(){
-    var queryURL = "https://maps.googleapis.com/maps/api/directions/json?origin=22.133541,-101.011290&destination=22.153591,-100.999651 &key=AIzaSyDheI0c_l_JaucbcUTP3nsDxYu2il8f1dM";
-    $.ajax({
-        url: queryURL,
-        method: "GET",
-        dataType: 'jsonp',
-        cache: false
-    }).done(function (data) {
-        console.log(JSON.stringify(data))
-    })
+// Create marker
+function CreateMarker(place) {
+    var marker = new google.maps.Marker({
+        map: map,
+        animation: google.maps.Animation.DROP,
+        position: place.geometry.location
+    });
+    google.maps.event.addListener(marker, 'click', function () {
+        infowindow.setContent(place.name);
+        infowindow.open(map, this);
+    });
 }
 
-//testData()
+
+// Calculate route
+function CalcRoute(startLat, startLon, endLat, endLong) {
+    $("html, body").animate({ scrollTop: 0 }, "slow");
+    var startMap = new google.maps.LatLng(startLat, startLon);
+    var endMap = new google.maps.LatLng(endLat, endLong);
+    var request = {
+        origin: startMap,
+        destination: endMap,
+        travelMode: 'DRIVING'
+    };
+    directionsService.route(request, function (result, status) {
+        if (status == 'OK') {
+            directionsDisplay.setDirections(result);
+        }
+    });
+}
